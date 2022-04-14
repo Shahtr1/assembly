0f17eb11b20f6754cff758b4422296d896a85f41
shahrukh.tramboos <shahrukh@pragtex.com>
2022-04-14 20:19:03 +0530

    Done real mode

--- PeachOS/boot.asm
+++ PeachOS/boot.asm
@@ -1,29 +1,63 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
+_start:
+    jmp short start
+    nop
+
+    times 33 db 0
+
 start:
-    mov si, message
+    jmp 0x7c0:step2
+
+step2:
+    cli
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax 
+    mov sp, 0x7c00
+    sti
+
+    mov ah, 2 ; READ SECTOR COMMAND
+    mov al, 1 ; ONE SECTOR TO READ
+    mov ch, 0 ; Cylinder low eight bits
+    mov cl, 2 ; Read sector two
+    mov dh, 0 ; Head number
+    ; we dont need to set dl, as its already been set for us
+    mov bx, buffer
+    int 0x13 ; to invoke the read command
+    jc error
+
+    mov si, buffer
+    call print
+    jmp $
+
+error:
+    mov si, error_message
     call print
     jmp $
 
 print:
-    mov bx, 0 ; paging
+    mov bx, 0
 .loop:
     lodsb
     cmp al, 0
     je .done
     call print_char
     jmp .loop
-.done: ; subroutine
+.done:
     ret
 
 print_char:
     mov ah, 0eh
-    int 0x10 ; we are calling a BIOS routine which outputs the char A to screen and 0eh is the command to do that
+    int 0x10
     ret
 
-message: db 'Hello World!', 0
+error_message: db 'failed to load sector', 0
 
-times 510-($ - $$) db 0 ; $ means current address and $$ means the first address of the current section. You have to understand that the times directive only operates on numbers and the difference of address ( $-$$ ) yields a number (Offset). So $-$$ gives you the offset from start to address of the currently executed instruction. If subtract that value from 510 you will get the offset from the address of the currently executed instruction to the 510th byte. So we now know how many bytes are there from the address of the currently executed instruction to the 510th byte. The times directive will now pad that number of bytes up to 510th byte with zeros.
-
+times 510-($ - $$) db 0
 dw 0xAA55
+
+buffer:
